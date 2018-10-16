e7113a1c6afb99b94b0a1d6005b928bce7e0b14e
Paul Mooney <daishisystems@users.noreply.github.com>
2018-10-16 16:43:10 +0100

    Added sort and max-diff features

--- com.dataflow.sample/src/test/java/com/dataflow/sample/OrderTest.java
+++ com.dataflow.sample/src/test/java/com/dataflow/sample/OrderTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,7 +12,6 @@ import org.junit.Test;
 public class OrderTest {
     @Test
     public void ordersAreSortedByCreated() {
-
         long firstTimestamp = 1539093918;
         long secondTimestamp = 1539093930;
         long thirdTimestamp = 1539093959;
@@ -28,48 +28,76 @@ public class OrderTest {
         orders.add(order1);
         orders.add(order2);
 
-        Order.SortOrders(orders);
+        List<Order> sortedOrders = sortOrders(orders.iterator());
 
-        assertTrue(orders.get(0).getCreated() == firstTimestamp);
-        assertTrue(orders.get(1).getCreated() == secondTimestamp);
-        assertTrue(orders.get(2).getCreated() == thirdTimestamp);
+        assertTrue(sortedOrders.get(0).getCreated() == firstTimestamp);
+        assertTrue(sortedOrders.get(1).getCreated() == secondTimestamp);
+        assertTrue(sortedOrders.get(2).getCreated() == thirdTimestamp);
     }
 
     @Test
     public void maxDifferenceBetweenOrderCreatedIsCalculated() {
-        long firstTimestamp = 1539093918;
-        long secondTimestamp = 1539093930;
-        long thirdTimestamp = 1539093959;
-
         Order order1 = new Order();
-        order1.setCreated((long) firstTimestamp);
+        order1.setCreated((long) 1539093918);
         Order order2 = new Order();
-        order2.setCreated((long) secondTimestamp);
+        order2.setCreated((long) 1539093930);
         Order order3 = new Order();
-        order3.setCreated((long) thirdTimestamp);
+        order3.setCreated((long) 1539093959);
 
         List<Order> orders = new ArrayList<>();
         orders.add(order1);
         orders.add(order2);
         orders.add(order3);
 
-        OrderDifference orderDifference = new OrderDifference();
-        Order o1 = null;
-        Order o2 = null;
-        long createdDifference = 0;
+        long maxDiff = maxDiff(orders);
+        assertTrue(maxDiff == 29);
+    }
 
-        for (int i = 1; i < orders.size(); i++) {
-            o1 = orders.get(i);
-            o2 = orders.get(i - 1);
-            createdDifference = o1.getCreated() - o2.getCreated();
+    @Test
+    public void maxDifferenceIsZeroForSingleItemOrderCollection() {
+        Order order = new Order();
 
-            if (createdDifference > orderDifference.getCreatedDifference()) {
-                orderDifference.setFirstOrder(o1);
-                orderDifference.setSecondOrder(o2);
-                orderDifference.setCreatedDifference(createdDifference);
-            }
+        List<Order> orders = new ArrayList<>();
+        orders.add(order);
+
+        long maxDiff = maxDiff(orders);
+        assertTrue(maxDiff == 0);
+    }
+
+    /**
+     * Returns the maximum difference between any pair of adjacent orders in a
+     * collection.
+     * 
+     * @param orders collection of orders to traverse
+     * @return maximum difference between any pair of adjacent ordersS
+     */
+    long maxDiff(List<Order> orders) {
+        Iterator<Order> iterator = orders.iterator();
+        long diff = 0;
+        if (!iterator.hasNext()) {
+            return diff;
         }
+        Order previous = iterator.next();
+        if (!iterator.hasNext()) {
+            return diff;
+        }
+        do {
+            Order current = iterator.next();
+            long currentDiff = current.getCreated() - previous.getCreated();
+            if (currentDiff > diff) {
+                diff = currentDiff;
+            }
+            previous = current;
+        } while (iterator.hasNext());
+        return diff;
+    }
 
-        assertTrue(orderDifference.getCreatedDifference() == 29);
+    List<Order> sortOrders(Iterator orders) {
+        List<Order> sortedOrders = new ArrayList<>();
+        while (orders.hasNext()) {
+            sortedOrders.add((Order) orders.next());
+        }
+        Collections.sort(sortedOrders, (o1, o2) -> new Long(o1.getCreated()).compareTo(new Long((o2.getCreated()))));
+        return sortedOrders;
     }
 }
